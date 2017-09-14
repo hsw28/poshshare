70f3059418f84df7a7dbd12156b20baa94260ebd
Hannah Wirtshafter <holdbackaglacier@yahoo.com>
2017-09-13 23:27:12 -0400

    makes interval between sharing random...
    ...so as to be more *stealth*

--- shareToFollowers.js
+++ shareToFollowers.js
@@ -2,18 +2,21 @@
 var buttons = document.querySelectorAll('.share');
 var i = buttons.length;
 
-setTimeout(function() { shareButton(); }, 500);
+x = Math.random() * (1200 - 400) + 400
+setTimeout(function() { shareButton(); }, x);
 function shareButton() {
   buttons[i - 1].click();
   if(i > 0) {
-    setTimeout(function() { shareToFollowers(); }, 500);
+    x = Math.random() * (1200 - 400) + 400
+    setTimeout(function() { shareToFollowers(); }, x);
   }
 }
 function shareToFollowers() {
   var sharebutton = document.querySelector(".pm-followers-share-link");
   sharebutton.click();
   if(i > 0) {
-    setTimeout(function() { shareButton(); }, 500);
+    x = Math.random() * (1200 - 400) + 400
+    setTimeout(function() { shareButton(); }, x);
     i--;
   }
 }
